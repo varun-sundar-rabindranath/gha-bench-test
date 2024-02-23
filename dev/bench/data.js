36856a9b6b8fd9732f24916eff3daf69d3ba037c
github-action-benchmark <github@users.noreply.github.com>
2024-02-23 21:36:50 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for d9f75526549c01e48bc779e2436db046f29b12bb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708723519089,
+  "lastUpdate": 1708724210497,
   "repoUrl": "https://github.com/varun-sundar-rabindranath/gha-bench-test",
   "entries": {
     "Benchmark": [
@@ -33,6 +33,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "My Custom Smaller Is Better Benchmark - Dummy 2",
             "value": 100,
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varun",
+            "username": "varun-sundar-rabindranath"
+          },
+          "committer": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varun",
+            "username": "varun-sundar-rabindranath"
+          },
+          "distinct": true,
+          "id": "d9f75526549c01e48bc779e2436db046f29b12bb",
+          "message": "update py bech",
+          "timestamp": "2024-02-24T03:06:27+05:30",
+          "tree_id": "578dffd665704f5926fe9af41671062e28537892",
+          "url": "https://github.com/varun-sundar-rabindranath/gha-bench-test/commit/d9f75526549c01e48bc779e2436db046f29b12bb"
+        },
+        "date": 1708724210137,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 1",
+            "value": "37",
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 2",
+            "value": "78",
             "unit": "Megabytes",
             "range": "3",
             "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
