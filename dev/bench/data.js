9d3828c74fe52c4fa7d4f0e26e2300f3b366f40a
github-action-benchmark <github@users.noreply.github.com>
2024-02-23 21:47:42 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 6a0ea34289cfa9e57c1acf4fbff4ff79f684afaa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708724210497,
+  "lastUpdate": 1708724862462,
   "repoUrl": "https://github.com/varun-sundar-rabindranath/gha-bench-test",
   "entries": {
     "Benchmark": [
@@ -69,6 +69,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "My Custom Smaller Is Better Benchmark - Dummy 2",
             "value": "78",
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varun",
+            "username": "varun-sundar-rabindranath"
+          },
+          "committer": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varun",
+            "username": "varun-sundar-rabindranath"
+          },
+          "distinct": true,
+          "id": "6a0ea34289cfa9e57c1acf4fbff4ff79f684afaa",
+          "message": "cosmetic change",
+          "timestamp": "2024-02-24T03:17:27+05:30",
+          "tree_id": "1a90e8c90c9382a3f05b392eb1ba28c0ffc1079a",
+          "url": "https://github.com/varun-sundar-rabindranath/gha-bench-test/commit/6a0ea34289cfa9e57c1acf4fbff4ff79f684afaa"
+        },
+        "date": 1708724861986,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 1",
+            "value": "63",
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 2",
+            "value": "12",
             "unit": "Megabytes",
             "range": "3",
             "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
