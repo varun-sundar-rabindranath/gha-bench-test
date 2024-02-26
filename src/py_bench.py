3cd9ed17db67423d5569bb90bcb8048ad383dc1c
varunsundar08 <varunsundar08@gmail.com>
2024-02-26 14:52:34 -0500

    Add a new dict metric

--- src/py_bench.py
+++ src/py_bench.py
@@ -21,6 +21,11 @@ def main(out_file):
         "range": "3",
         "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
     })
+    dicts.append({
+        "name": "My Custom Smaller Is Better Benchmark - Dummy 3",
+        "unit": "Time(ms)",
+        "value": f"{random.randint(10, 100)}"
+    })
 
     with open(out_file, "w+") as f:
         json.dump(dicts, f, sort_keys=True, indent=4)
