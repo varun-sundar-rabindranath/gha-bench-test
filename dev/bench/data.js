6e82ac8aabf70bfe45807ba8c7a53b2022008e39
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 04:33:19 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for de335041cd597433b5991d77c42c40077a033571

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709785997559,
+  "lastUpdate": 1709785999080,
   "repoUrl": "https://github.com/varun-sundar-rabindranath/gha-bench-test",
   "entries": {
     "Benchmark": [
@@ -640,6 +640,47 @@ window.BENCHMARK_DATA = {
             "unit": "",
             "range": "3",
             "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varun",
+            "username": "varun-sundar-rabindranath"
+          },
+          "committer": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varun",
+            "username": "varun-sundar-rabindranath"
+          },
+          "distinct": true,
+          "id": "de335041cd597433b5991d77c42c40077a033571",
+          "message": "add empty unit",
+          "timestamp": "2024-03-06T23:33:00-05:00",
+          "tree_id": "73ae9bb73c0731314dcb568c19c6f304011a7040",
+          "url": "https://github.com/varun-sundar-rabindranath/gha-bench-test/commit/de335041cd597433b5991d77c42c40077a033571"
+        },
+        "date": 1709785998185,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 1",
+            "value": "11",
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 2",
+            "value": "57",
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 3",
+            "value": "32",
+            "unit": "Time(ms)"
           }
         ]
       }
