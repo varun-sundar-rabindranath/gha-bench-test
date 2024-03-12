3123daaa66c2db8a37681cb0a1f16d0041af526b
github-action-benchmark <github@users.noreply.github.com>
2024-03-12 20:00:12 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fb7ad41bcdbbbd39d22529820bd5233a8e15f494

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1710273612082,
+  "repoUrl": "https://github.com/varun-sundar-rabindranath/gha-bench-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varunsundar08",
+            "username": "varun-sundar-rabindranath"
+          },
+          "committer": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varunsundar08",
+            "username": "varun-sundar-rabindranath"
+          },
+          "distinct": true,
+          "id": "fb7ad41bcdbbbd39d22529820bd5233a8e15f494",
+          "message": "change gh-pages-branch",
+          "timestamp": "2024-03-12T15:59:50-04:00",
+          "tree_id": "aee14748fa277f97064eb11f7a68e9f08174f703",
+          "url": "https://github.com/varun-sundar-rabindranath/gha-bench-test/commit/fb7ad41bcdbbbd39d22529820bd5233a8e15f494"
+        },
+        "date": 1710273611609,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "pybench2 Dummy 1",
+            "value": "31",
+            "unit": "Percent"
+          },
+          {
+            "name": "pybench2 Dummy 2",
+            "value": "70",
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          },
+          {
+            "name": "no-unit",
+            "value": "30",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          },
+          {
+            "name": "empty-unit",
+            "value": "89",
+            "unit": "",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      }
+    ]
+  }
+}
