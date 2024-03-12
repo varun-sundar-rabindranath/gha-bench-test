6167986eb51a891eec16b5cbe637e1957e609791
github-action-benchmark <github@users.noreply.github.com>
2024-03-12 20:00:13 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for fb7ad41bcdbbbd39d22529820bd5233a8e15f494

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710273612082,
+  "lastUpdate": 1710273613296,
   "repoUrl": "https://github.com/varun-sundar-rabindranath/gha-bench-test",
   "entries": {
     "Benchmark": [
@@ -49,6 +49,47 @@ window.BENCHMARK_DATA = {
             "unit": "",
             "range": "3",
             "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varunsundar08",
+            "username": "varun-sundar-rabindranath"
+          },
+          "committer": {
+            "email": "varunsundar08@gmail.com",
+            "name": "varunsundar08",
+            "username": "varun-sundar-rabindranath"
+          },
+          "distinct": true,
+          "id": "fb7ad41bcdbbbd39d22529820bd5233a8e15f494",
+          "message": "change gh-pages-branch",
+          "timestamp": "2024-03-12T15:59:50-04:00",
+          "tree_id": "aee14748fa277f97064eb11f7a68e9f08174f703",
+          "url": "https://github.com/varun-sundar-rabindranath/gha-bench-test/commit/fb7ad41bcdbbbd39d22529820bd5233a8e15f494"
+        },
+        "date": 1710273612478,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 1",
+            "value": "66",
+            "unit": "Percent"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 2",
+            "value": "37",
+            "unit": "Megabytes",
+            "range": "3",
+            "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Dummy 3",
+            "value": "68",
+            "unit": "Time(ms)"
           }
         ]
       }
