4c926fc7698422fd38c34620f44087a3e81fe66d
varun <varunsundar08@gmail.com>
2024-03-18 14:56:47 -0400

    up the ante

--- src/metrics.py
+++ src/metrics.py
@@ -13,12 +13,12 @@ def main(out_file, metric_type):
         dicts.append({
             "name": "bigger_is_better",
             "unit": "ms",
-            "value": 100})
+            "value": 105})
     elif metric_type == 'customSmallerIsBetter':
         dicts.append({
             "name": "smaller_is_better",
             "unit": "ms",
-            "value": 100})
+            "value": 95})
 
     with open(out_file, "w+") as f:
         json.dump(dicts, f, sort_keys=True, indent=4)
